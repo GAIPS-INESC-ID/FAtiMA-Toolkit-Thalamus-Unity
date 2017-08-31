4389ed264ba3ae0ab85004eb6dcc37e0f0991dc6
Samuel Mascarenhas <samuel.fm@gmail.com>
2017-08-31 22:28:55 +0100

    Initial UI Creation

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonTest : MonoBehaviour
+{
+    public Slider Slider;
+
+    public void DoSomething()
+    {
+        Debug.Log(Slider.value.ToString());
+    }
+    public void DoSomethingWithASlider(Slider slider)
+    {
+        Debug.Log(Slider.value.ToString());
+    }
+}
